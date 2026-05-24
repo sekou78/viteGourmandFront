a435da73e522e253e8b9ca0adc2c02c98e35d3ff
sekou78 <s.cisse78@gmail.com>
2026-05-24 16:26:38 +0200

    Ajout du système de routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,7 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [new Route("/", "Accueil", "/pages/home.html", [])];
+
+//Le titre s'affiche comme ceci: Route.titre-websiteName
+export const websiteName = "Dounkafa";
