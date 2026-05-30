8f4ad0657e06170d491db1d2dc06de5dc22a765c
sekou78 <s.cisse78@gmail.com>
2026-05-30 14:51:43 +0200

    Ajout de la page menu

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,20 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html", [], "/js/home.js"),
+  new Route(
+    "/listeMenu",
+    "Liste des menus",
+    "/pages/menu/listeMenu.html",
+    [],
+    "/js/menu/listeMenu.js",
+  ),
+  new Route(
+    "/detailMenu",
+    "Détail du menu",
+    "/pages/menu/detailMenu.html",
+    [],
+    "/js/menu/detailMenu.js",
+  ),
 ];
 
 //Le titre s'affiche comme ceci: Route.titre-websiteName
